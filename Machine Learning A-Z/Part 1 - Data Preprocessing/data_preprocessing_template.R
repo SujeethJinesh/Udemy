1e9e59e35f14ccd6151ccc66a8979bdda97c9441
Sujeeth Jinesh <sujeethjinesh@gmail.com>
2017-05-13 18:54:12 -0700

    Add missing data code

--- Machine Learning A-Z/Part 1 - Data Preprocessing/data_preprocessing_template.R
+++ Machine Learning A-Z/Part 1 - Data Preprocessing/data_preprocessing_template.R
@@ -3,14 +3,11 @@
 # Importing the dataset
 dataset = read.csv('Data.csv')
 
-# Splitting the dataset into the Training set and Test set
-# install.packages('caTools')
-library(caTools)
-set.seed(123)
-split = sample.split(dataset$DependentVariable, SplitRatio = 0.8)
-training_set = subset(dataset, split == TRUE)
-test_set = subset(dataset, split == FALSE)
-
-# Feature Scaling
-# training_set = scale(training_set)
-# test_set = scale(test_set)
+#Now we need to avg the values of the features in order to
+#put them into the null/NaN values.
+dataset$Age = ifelse(is.na(dataset$Age),
+                     ave(dataset$Age, FUN = function(x) mean(x, na.rm=TRUE)),
+                     dataset$Age)
+dataset$Salary = ifelse(is.na(dataset$Salary),
+                     ave(dataset$Salary, FUN = function(x) mean(x, na.rm=TRUE)),
+                     dataset$Salary)
