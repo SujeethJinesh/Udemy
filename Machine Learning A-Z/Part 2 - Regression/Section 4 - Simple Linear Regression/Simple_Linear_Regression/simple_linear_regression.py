4fe572592a3a425d8adbcdd9c6c313aa1c7107fd
Sujeeth Jinesh <sujeethjinesh@gmail.com>
2017-05-15 00:12:38 -0700

    Finish Linear Regression

---
+++ Machine Learning A-Z/Part 2 - Regression/Section 4 - Simple Linear Regression/Simple_Linear_Regression/simple_linear_regression.py
@@ -0,0 +1,49 @@
+# Simple Linear Regression
+# Data Preprocessing Template
+
+##########################################################################
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Salary_Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+
+# Feature Scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+sc_y = StandardScaler()
+y_train = sc_y.fit_transform(y_train)"""
+
+                            
+        #Preprocessing Template
+##########################################################################
+
+
+#Here we are creating the regression model. It uses the ordinary square way,
+#by minimizing the difference of the squares of the data.
+from sklearn.linear_model import LinearRegression
+
+regressor = LinearRegression()
+regressor.fit(X_train, y_train) #Here we make the model
+
+#Now we want to predict some values. and compare it to the y_test
+y_pred = regressor.predict(X_test)
+
+#Now we want to visualize the training set results.
+plt.scatter(X_train, y_train, color='red')
+plt.plot(X_train, regressor.predict(X_train), color='blue')
+plt.title('Salary vs. Experience (Training set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
