ae777005ce60dc76251d3230fadb4ebb0376d18a
Sujeeth Jinesh <sujeethjinesh@gmail.com>
2017-05-15 12:34:57 -0700

    Begin Multiple Regression

--- Machine Learning A-Z/Part 2 - Regression/Section 4 - Simple Linear Regression/Simple_Linear_Regression/simple_linear_regression.py
+++ Machine Learning A-Z/Part 2 - Regression/Section 4 - Simple Linear Regression/Simple_Linear_Regression/simple_linear_regression.py
@@ -47,3 +47,14 @@ plt.title('Salary vs. Experience (Training set)')
 plt.xlabel('Years of Experience')
 plt.ylabel('Salary')
 plt.show()
+
+#Now we want to visualize the test set results.
+plt.scatter(X_test, y_test, color='red')
+plt.plot(X_train, regressor.predict(X_train), color='blue')
+plt.title('Salary vs. Experience (Test set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+
+
