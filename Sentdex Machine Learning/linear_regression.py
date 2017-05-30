15755091cd82d6447a7f4b8090bd0d90d4a491d7
Sujeeth Jinesh <sjinesh@hearsaycorp.com>
2017-05-30 00:46:26 -0700

    Second Lesson

--- Sentdex Machine Learning/linear_regression.py
+++ Sentdex Machine Learning/linear_regression.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import quandl
+import quandl, math
 
 df = quandl.get("WIKI/GOOGL") # importing dataset
 
@@ -13,3 +13,13 @@ df['Percent_Change'] = (df['Adj. Close'] - df['Adj. Open'])/df['Adj. Open'] * 10
 df = df[['Adj. Close', 'High_Low_Percent_Difference', 'Percent_Change', 'Adj. Volume']] # printing out new calculations
 
 # print(df.head())
+
+forecast_col = 'Adj. Close' # label for what we want to predict
+
+df.fillna(-9999, inplace=True) # replacing the nans with outlier data, might want to consider the avg of the column as well.
+
+forecast_out = int(math.ceil(0.01*len(df))) # want to predict a data point 10% ahead. i.e. if you have 10 days of data, you can predict 1 day in advance
+
+df['label'] = df[forecast_col].shift(-forecast_out) # ??
+df.dropna(inplace=True)
+print(df.head())
