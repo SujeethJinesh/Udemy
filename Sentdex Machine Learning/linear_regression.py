7dbefea91d75e52081721c995644411c9fe65203
Sujeeth Jinesh <sujeethjinesh@gmail.com>
2017-05-30 01:20:29 -0700

    Basic Linear Regression

--- Sentdex Machine Learning/linear_regression.py
+++ Sentdex Machine Learning/linear_regression.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import quandl, math
+import numpy as np
+from sklearn import preprocessing, cross_validation, svm
+from sklearn.linear_model import LinearRegression
 
 df = quandl.get("WIKI/GOOGL") # importing dataset
 
@@ -22,4 +25,19 @@ forecast_out = int(math.ceil(0.01*len(df))) # want to predict a data point 10% a
 
 df['label'] = df[forecast_col].shift(-forecast_out) # ??
 df.dropna(inplace=True)
-print(df.head())
+# print(df.head())
+
+X = np.array(df.drop(['label'], 1)) # features
+y = np.array(df['label']) #
+
+X = preprocessing.scale(X) # normalizes the features
+y = np.array(df['label'])
+
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2) # Splitting into training(80%) and testing(20%)
+
+classifier = LinearRegression() # classifier we are choosing
+
+classifier.fit(X_train, y_train) # fitting our classifier to the data
+accuracy = classifier.score(X_test, y_test) # scoring our data against the testing data
+
+print(accuracy)
