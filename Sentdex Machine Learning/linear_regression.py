126a7990d3b0257391967c8c7374a7ac554e6aeb
Sujeeth Jinesh <sujeethjinesh@gmail.com>
2017-06-04 19:47:45 -0700

    Add GOOG Stock Predictor

--- Sentdex Machine Learning/linear_regression.py
+++ Sentdex Machine Learning/linear_regression.py
@@ -1,10 +1,14 @@
 import pandas as pd
-import quandl, math
+import quandl, math, datetime
 import numpy as np
 from sklearn import preprocessing, cross_validation, svm
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt 
+from matplotlib import style
 
-df = quandl.get("WIKI/GOOGL") # importing dataset
+style.use('ggplot') # make the plot look actually good
+
+df = quandl.get("WIKI/GOOG") # importing dataset of stock you wish to use
 
 # print(df.head())
 
@@ -19,27 +23,54 @@ df = df[['Adj. Close', 'High_Low_Percent_Difference', 'Percent_Change', 'Adj. Vo
 
 forecast_col = 'Adj. Close' # label for what we want to predict
 
-df.fillna(-9999, inplace=True) # replacing the nans with outlier data, might want to consider the avg of the column as well.
+# df.fillna(-9999, inplace=True) # replacing the nans with outlier data, might want to consider the avg of the column as well.
+df.apply(lambda x: x.fillna(x.mean()),axis=0) #this might help with replacing with the avg --> looks like it has very little effect on pred
 
-forecast_out = int(math.ceil(0.01*len(df))) # want to predict a data point 10% ahead. i.e. if you have 10 days of data, you can predict 1 day in advance
+forecast_out = int(math.ceil(0.01*len(df))) # want to predict a data point 1% ahead. i.e. if you have 10 days of data, you can predict 1 day in advance
 
-df['label'] = df[forecast_col].shift(-forecast_out) # ??
-df.dropna(inplace=True)
+df['label'] = df[forecast_col].shift(-forecast_out) # Shifting out how far we wanna go?
 # print(df.head())
 
 X = np.array(df.drop(['label'], 1)) # features
-y = np.array(df['label']) #
-
 X = preprocessing.scale(X) # normalizes the features
+X = X[:-forecast_out]
+X_lately = X[-forecast_out:]
+
+df.dropna(inplace=True)
+y = np.array(df['label']) #
 y = np.array(df['label'])
 
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2) # Splitting into training(80%) and testing(20%)
 
-classifier = LinearRegression() # classifier we are choosing
+# classifier = LinearRegression() # classifier we are choosing
 # classifier = svm.SVR() # if you wanna switch the classifier, you can do this.
 # classifier = svm.SVR(kernel='poly') # can switch around the type as well
+classifier = svm.SVR(kernel='rbf', C=1e3, gamma=0.1) # Seems like a decent fit, might wanna have multiple ones.
 
 classifier.fit(X_train, y_train) # fitting our classifier to the data
 accuracy = classifier.score(X_test, y_test) # scoring our data against the testing data
 
-print(accuracy)
+forecast_set = classifier.predict(X_lately)
+
+# print(forecast_set, accuracy, forecast_out)
+df['Forecast'] = np.nan
+
+last_date = df.iloc[-1].name
+last_unix = last_date.timestamp()
+one_day = 86400
+next_unix = last_unix + one_day
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += one_day
+    df.loc[next_date] = [np.nan for _ in range(len(df.columns)-1)] + [i]
+
+print(df.tail())
+
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.title("GOOG")
+plt.show()
